71134be1d4df4840cb8d904a2140dd74edc362fc
Nawsen <wannesvandorpe@hotmail.com>
2015-10-10 12:31:11 +0200

    start login + basic mvc structure

---
+++ Web/app/scripts/app.js
@@ -0,0 +1,23 @@
+/**
+ * Created by wannes on 9/10/2015.
+ */
+var app = angular.module('eva', ['ui.router']);
+
+    app.config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+
+
+
+        $stateProvider
+            .state('login', {
+                url: '/login',
+                templateUrl: 'views/login.html',
+                controller: 'LoginCtrl'
+            })
+            .state('dashboard', {
+                url: '/dashboard',
+                templateUrl: 'views/dashboard.html',
+                controller: 'DashboardCtrl'
+            });
+        $urlRouterProvider.otherwise('/login');
+
+    }]);
---
+++ Web/app/scripts/controllers/dashboard.js
@@ -0,0 +1,8 @@
+/**
+ * Created by wannes on 9/10/2015.
+ */
+angular.module('eva').controller('DashboardCtrl', function($scope, $location){
+
+
+
+});
