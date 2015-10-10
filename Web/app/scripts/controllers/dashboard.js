3ba392f7876186925ef351492e0947da068668ec
Nawsen <wannesvandorpe@hotmail.com>
2015-10-10 15:15:42 +0200

    dashboard mockup

--- Web/app/scripts/controllers/dashboard.js
+++ Web/app/scripts/controllers/dashboard.js
@@ -1,8 +1,8 @@
 /**
  * Created by wannes on 9/10/2015.
  */
-angular.module('eva').controller('DashboardCtrl', function($scope, $location){
+angular.module('eva').controller('DashboardCtrl', ['$scope', '$location', function ($scope, $location) {
+    $scope.user = {'firstname': 'voornaam', 'lastname': 'achternaam', 'avatar': 'http://placehold.it/150x150'};
 
 
-
-});
+}]);
