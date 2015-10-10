b777ce0d162ed30642c6367d0cc4f5b1b4ad43f9
Mathias Bulté <bulte.mathias@gmail.com>
2015-10-10 15:27:07 +0200

    Register view aangemaakt

--- Web/app/scripts/controllers/register.js
+++ Web/app/scripts/controllers/register.js
@@ -1,11 +1,11 @@
 /**
  * Created by wannes on 9/10/2015.
  */
-angular.module('eva').controller('LoginCtrl', ['$scope', '$location', function ($scope, $location) {
+angular.module('eva').controller('RegisterCtrl', ['$scope', '$location', function ($scope, $location) {
     $scope.login = function () {
         //TODO check login info with backend
         //for now basic login checker
-        if ($scope.email === "demo" && $scope.password === "demo") {
+        /*if ($scope.email === "demo" && $scope.password === "demo") {
             $scope.emailwarn = "";
             $scope.passwordwarn = "";
             $location.path('/dashboard');
@@ -17,18 +17,10 @@ angular.module('eva').controller('LoginCtrl', ['$scope', '$location', function (
         }
         $scope.email = "";
         $scope.password = "";
-        return false;
+        return false;*/
 
     };
-    $scope.facebooklogin = function () {
-        //TODO implement facebook api
-        $location.path('/dashboard');
-        return false;
-    }
-    $scope.register = function () {
-        //TODO implement facebook api
-        $location.path('/register');
-        return false;
-    }
+
+
 
 }]);
