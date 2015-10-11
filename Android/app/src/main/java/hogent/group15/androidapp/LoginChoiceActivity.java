a1a6d14bae9ecd9f5d77fe7bfbec435078447a6c
Limnic <brent.couck.u9080@student.hogent.be>
2015-10-11 14:24:06 +0200

    LoginChoiceActivity update

--- Android/app/src/main/java/hogent/group15/androidapp/LoginChoiceActivity.java
+++ Android/app/src/main/java/hogent/group15/androidapp/LoginChoiceActivity.java
@@ -1,7 +1,9 @@
 package hogent.group15.androidapp;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
 
 public class LoginChoiceActivity extends AppCompatActivity {
@@ -10,6 +12,10 @@ public class LoginChoiceActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_choice);
-        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+    }
+
+    public void onAlternativeLoginClick(View v) {
+        Intent i = new Intent(this, LoginActivity.class);
+        startActivity(i);
     }
 }
