05af79edfc59d91cc6843821dbc9d76b013604eb
frederikdesmedt <frederik.de.smedt@hotmail.com>
2015-10-12 22:57:29 +0200

    Merge commit

--- Android/app/src/main/java/hogent/group15/ui/LoginChoiceActivity.java
+++ Android/app/src/main/java/hogent/group15/ui/LoginChoiceActivity.java
@@ -1,10 +1,9 @@
-package hogent.group15.androidapp;
+package hogent.group15.ui;
 
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.view.Window;
 
 public class LoginChoiceActivity extends AppCompatActivity {
 
