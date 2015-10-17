85b9469c221ab5da10b04b50184926fadff4d7a2
frederikdesmedt <frederik.de.smedt@hotmail.com>
2015-10-17 14:14:41 +0200

    Basic registration backend implementation

---
+++ Backend/src/java/hogent/group15/configuration/Users.java
@@ -0,0 +1,62 @@
+package hogent.group15.configuration;
+
+import hogent.group15.User;
+import java.net.URI;
+import java.util.Set;
+import javax.enterprise.context.Dependent;
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import javax.transaction.Transactional;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validator;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.WebApplicationException;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.Response.Status;
+
+/**
+ *
+ * @author Frederik
+ */
+@Path("users")
+@Dependent
+public class Users {
+
+    @PersistenceContext
+    private EntityManager em;
+
+    @Context
+    private Validator validator;
+    
+    @GET
+    @Path("hello")
+    public String hello() {
+	return "hello";
+    }
+    
+    @Path("register")
+    @Transactional
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Response register(User user) {
+	User dbUser = em.find(User.class, user.getEmail());
+	if (dbUser != null) {
+	    return Response.status(Response.Status.BAD_REQUEST).entity("The given e-mail is already in use.").build();
+	} else {
+	    Set<ConstraintViolation<User>> violations = validator.validate(user);
+	    if (!violations.isEmpty()) {
+		StringBuilder builder = new StringBuilder();
+		violations.stream().map(cv -> cv.getMessage() + " ").forEach(builder::append);
+		throw new WebApplicationException(Response.status(Status.BAD_REQUEST).entity(builder.toString()).build());
+	    } else {
+		em.persist(user);
+		return Response.created(URI.create("/users/me")).build();
+	    }
+	}
+    }
+}
