18150f588412c734520288be581a1ce30decc0d6
Limnic <brent.couck.u9080@student.hogent.be>
2015-10-18 15:23:45 +0200

    Login style rework and facebook login button

--- Android/app/src/main/java/hogent/group15/ui/LoginActivity.java
+++ Android/app/src/main/java/hogent/group15/ui/LoginActivity.java
@@ -4,13 +4,18 @@ import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+
+import com.facebook.FacebookSdk;
 
 public class LoginActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        FacebookSdk.sdkInitialize(getApplicationContext());
         setContentView(R.layout.activity_login);
+        //((EditText)findViewById(R.id.login_email)).setError("Mail cannot be empty!");
     }
 
     public void onLogin(View v) {
