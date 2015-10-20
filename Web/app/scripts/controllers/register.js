e560349896437a316730dc2cf606d0c8bd4df4f1
Mathias Bulté <bulte.mathias@gmail.com>
2015-10-20 18:23:35 +0200

    Validatie op: - E-mailadres - Wachtwoord

--- Web/app/scripts/controllers/register.js
+++ Web/app/scripts/controllers/register.js
@@ -2,24 +2,26 @@
  * Created by wannes on 9/10/2015.
  */
 angular.module('eva').controller('RegisterCtrl', ['$scope', '$location', function ($scope, $location) {
-    $scope.login = function () {
-        //TODO check login info with backend
-        //for now basic login checker
-        /*if ($scope.email === "demo" && $scope.password === "demo") {
-            $scope.emailwarn = "";
-            $scope.passwordwarn = "";
-            $location.path('/dashboard');
-        } else {
-            $scope.messages = [
-                {'emailerror': 'Wrong email!'},
-                {'passworderror': 'Wrong password!'}
-            ];
-        }
-        $scope.email = "";
-        $scope.password = "";
-        return false;*/
+    $scope.form = {
 
+    }
+    $scope.user = {
+        email: "",
+        password:"sssss",
+        repeatPassword:"ssss",
+        firstName: "",
+        lastName: ""
     };
+    $scope.validatePassword = function () {
+        console.log(registerForm.email.$valid);
+        if (($scope.user.password === $scope.user.repeatPassword) && $scope.user.password.length >= 5 && $scope.form.email.$valid) {
+            console.log('test');
+            return false;
+        } else {
+            console.log('test2');
+            return true;
+        }
+    }
 
 
 
