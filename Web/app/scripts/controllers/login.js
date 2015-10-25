356e85f510f9430e71edb7e5992bdebb6b59bb66
Nawsen <wannesvandorpe@hotmail.com>
2015-10-25 14:01:50 +0100

    login now sends request to backend

--- Web/app/scripts/controllers/login.js
+++ Web/app/scripts/controllers/login.js
@@ -1,23 +1,31 @@
 /**
  * Created by wannes on 9/10/2015.
  */
-angular.module('eva').controller('LoginCtrl', ['$scope', '$location', function ($scope, $location) {
+angular.module('eva').controller('LoginCtrl', ['$scope', '$http','$location', function ($scope, $http, $location) {
     $scope.login = function () {
-        //TODO check login info with backend
-        //for now basic login checker
-        if ($scope.email === "demo" && $scope.password === "demo") {
+        //TODO implement jsonwebtokens
+
+        var dataObj = {
+            email : $scope.email,
+            password : $scope.password
+        };
+        var res = $http.post('http://178.62.232.69/backend/api/user/login/', dataObj);
+        res.success(function(data, status, headers, config) {
+            $location.path('/dashboard');
+            localStorage.setItem("email", $scope.email);
             angular.element(document.querySelector('#emailwarn')).text("");
             angular.element(document.querySelector('#passwordwarn')).text("");
-            $location.path('/dashboard');
-        } else {
+        });
+        res.error(function(data, status, headers, config) {
             angular.element(document.querySelector('#emailwarn')).text("Wrong email!");
             angular.element(document.querySelector('#passwordwarn')).text("Wrong password!");
-        }
+        });
+
         $scope.email = "";
         $scope.password = "";
         return false;
 
-    };
+    }
     $scope.facebooklogin = function () {
         //TODO implement facebook api
         $location.path('/dashboard');
@@ -28,10 +36,6 @@ angular.module('eva').controller('LoginCtrl', ['$scope', '$location', function (
         $location.path('/register');
         return false;
     }
-    };
-    $scope.register = function(){
-        $location.path('/register');
-        return false;
-    };
+
 
 }]);
