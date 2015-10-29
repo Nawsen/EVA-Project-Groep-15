57cab305d2c90a5740db7b5e88f8929f890193ac
Mathias Bulté <bulte.mathias@gmail.com>
2015-10-29 12:48:19 +0100

    JWT toegevoegd aan login en register, authentication factory aangemaakt

--- Web/app/scripts/controllers/login.js
+++ Web/app/scripts/controllers/login.js
@@ -1,7 +1,9 @@
 /**
  * Created by wannes on 9/10/2015.
  */
-angular.module('eva').controller('LoginCtrl', ['$scope', '$http','$location', function ($scope, $http, $location) {
+angular.module('eva').controller('LoginCtrl',
+    ['$scope', '$http','$location', '$state', 'auth',
+    function ($scope, $http, $location, $state, auth) {
     $scope.login = function () {
         //TODO implement jsonwebtokens
 
@@ -9,17 +11,16 @@ angular.module('eva').controller('LoginCtrl', ['$scope', '$http','$location', fu
             email : $scope.email,
             password : $scope.password
         };
-        var res = $http.post('http://178.62.232.69/backend/api/user/login/', dataObj);
-        res.success(function(data, status, headers, config) {
-            $location.path('/dashboard');
-            localStorage.setItem("email", $scope.email);
-            angular.element(document.querySelector('#emailwarn')).text("");
-            angular.element(document.querySelector('#passwordwarn')).text("");
-        });
-        res.error(function(data, status, headers, config) {
+        auth.login(dataObj).error(function (error) {
+            $scope.error = error;
             angular.element(document.querySelector('#emailwarn')).text("Wrong email!");
             angular.element(document.querySelector('#passwordwarn')).text("Wrong password!");
+        }).then(function () {
+            angular.element(document.querySelector('#emailwarn')).text("");
+            angular.element(document.querySelector('#passwordwarn')).text("");
+            $state.go('dashboard');
         });
+
 
         $scope.email = "";
         $scope.password = "";
@@ -28,12 +29,12 @@ angular.module('eva').controller('LoginCtrl', ['$scope', '$http','$location', fu
     }
     $scope.facebooklogin = function () {
         //TODO implement facebook api
-        $location.path('/dashboard');
+        $state.go('/dashboard');
         return false;
     }
     $scope.register = function () {
         //TODO implement facebook api
-        $location.path('/register');
+        $state.go('/register');
         return false;
     }
 
---
+++ Web/app/scripts/factories/authentication.js
@@ -0,0 +1,55 @@
+/**
+ * Created by Mathias on 29-Oct-15.
+ */
+
+app.factory('auth', ['$http', '$window', function($http, $window){
+    var auth = {};
+    auth.saveToken = function (token){
+        $window.localStorage['eva-token'] = token;
+    };
+
+    auth.getToken = function (){
+        return $window.localStorage['eva-token'];
+    }
+    auth.isLoggedIn = function(){
+        var token = auth.getToken();
+
+        if(token){
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.exp > Date.now() / 1000;
+        } else {
+            return false;
+        }
+    };
+    auth.currentUser = function(){
+        if(auth.isLoggedIn()){
+            var token = auth.getToken();
+            var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+            return payload.username;
+        }
+    };
+    auth.register = function(user){
+        return $http.post('https://bitcode.io:8080/backend/api/user/register', user).success(function(data){
+            auth.saveToken(data.token);
+        });
+    };
+
+    auth.login = function(user){
+        return $http.post('https://bitcode.io:8080/backend/api/user/login', user).success(function(data){
+            auth.saveToken(data.token);
+        });
+    };
+
+    auth.logOut = function(){
+        $window.localStorage.removeItem('eva-token');
+    };
+
+
+
+
+
+    return auth;
+}])
+
