64e854bfa6bdc742942298a8604aab18622616f0
Nawsen <wannesvandorpe@hotmail.com>
2015-10-30 21:31:14 +0100

    seperate user & challenges api

--- Backend/src/java/hogent/group15/configuration/Users.java
+++ Backend/src/java/hogent/group15/configuration/Users.java
@@ -4,10 +4,16 @@ import hogent.group15.Challenge;
 import hogent.group15.ChallengeCache;
 import hogent.group15.DailyChallenges;
 import hogent.group15.User;
+import io.jsonwebtoken.JwtBuilder;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
 import java.net.URI;
+import java.security.Key;
 import java.util.Arrays;
+import java.util.Date;
 import java.util.List;
 import java.util.Set;
+import javax.crypto.spec.SecretKeySpec;
 import javax.enterprise.context.Dependent;
 import javax.inject.Inject;
 import javax.persistence.EntityManager;
@@ -17,6 +23,7 @@ import javax.validation.ConstraintViolation;
 import javax.validation.Validator;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.GET;
+import javax.ws.rs.HeaderParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
@@ -27,6 +34,8 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+import javax.xml.bind.DatatypeConverter;
+import static org.eclipse.persistence.sessions.remote.corba.sun.TransporterHelper.id;
 
 /**
  *
@@ -50,20 +59,20 @@ public class Users {
     @Consumes(MediaType.APPLICATION_JSON)
     @Transactional
     public Response register(User user) {
-	User dbUser = em.find(User.class, user.getEmail());
-	if (dbUser != null) {
-	    return Response.status(Response.Status.BAD_REQUEST).entity("used").build();
-	} else {
-	    Set<ConstraintViolation<User>> violations = validator.validate(user);
-	    if (!violations.isEmpty()) {
-		StringBuilder builder = new StringBuilder();
-		violations.stream().map(cv -> cv.getMessage() + " ").forEach(builder::append);
-		throw new WebApplicationException(Response.status(Status.BAD_REQUEST).entity(builder.toString()).build());
-	    } else {
-		em.persist(user);
-		return Response.created(URI.create("/users/me")).build();
-	    }
-	}
+        User dbUser = em.find(User.class, user.getEmail());
+        if (dbUser != null) {
+            return Response.status(Response.Status.BAD_REQUEST).entity("used").build();
+        } else {
+            Set<ConstraintViolation<User>> violations = validator.validate(user);
+            if (!violations.isEmpty()) {
+                StringBuilder builder = new StringBuilder();
+                violations.stream().map(cv -> cv.getMessage() + " ").forEach(builder::append);
+                throw new WebApplicationException(Response.status(Status.BAD_REQUEST).entity(builder.toString()).build());
+            } else {
+                em.persist(user);
+                return Response.created(URI.create("/users/me")).build();
+            }
+        }
     }
 
     @Path("login")
@@ -71,103 +80,32 @@ public class Users {
     @POST
     @Produces(MediaType.APPLICATION_JSON)
     @Consumes(MediaType.APPLICATION_JSON)
-    public Response login(User user) {
-	User dbUser = em.find(User.class, user.getEmail());
-	if (dbUser != null && dbUser.getPassword().equals(user.getPassword())) {
-	    //TODO implement jsonwebtoken
-	    return Response.noContent().build();
-	} else {
-	    return Response.status(Status.UNAUTHORIZED).build();
-	}
+    public String login(User user) {
+        User dbUser = em.find(User.class, user.getEmail());
+        if (dbUser != null && dbUser.checkPassword(user.getPassword())) {
+            //TODO implement jsonwebtoken
+            return getToken();
+        } else {
+            throw new WebApplicationException(Response.status(Status.UNAUTHORIZED).build());
+        }
 
     }
 
-    @Path("{email}/completed")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public List<Challenge> getCompletedChallenges(@PathParam("email") String email) {
-	User user = em.find(User.class, email);
-	return user.getCompletedChallenges();
+    public String getToken() {
+        //The JWT signature algorithm we will be using to sign the token
+        SignatureAlgorithm signatureAlgorithm = SignatureAlgorithm.HS256;
+
+        long nowMillis = System.currentTimeMillis();
+        Date now = new Date(nowMillis);
+
+        //We will sign our JWT with our ApiKey secret
+        byte[] apiKeySecretBytes = DatatypeConverter.parseBase64Binary("secret");
+        Key signingKey = new SecretKeySpec(apiKeySecretBytes, signatureAlgorithm.getJcaName());
+        JwtBuilder builder = Jwts.builder().setId("demo@demo.be")
+                                .setIssuedAt(now)
+                .signWith(signatureAlgorithm, signingKey);
+       
+        return "{ \"token\": \"" + builder.compact() + "\" }";
     }
 
-    @Path("{email}/daily")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    @Transactional
-    public List<Challenge> getDailyChallenges(@PathParam("email") String email) {
-	User user = em.find(User.class, email);
-	DailyChallenges ch = cache.createDailyChallenges(user);
-	em.persist(user);
-	return Arrays.asList(new Challenge[]{ch.getFirst(), ch.getSecond(), ch.getThird()});
-    }
-
-    @Path("{email}/{challengeID}")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Challenge getChallengeDetails(@PathParam("email") String email, @PathParam("challengeID") int id) {
-	User user = em.find(User.class, email);
-	Challenge challenge = cache.getChallenge(user, id);
-
-	if (challenge != null) {
-	    return challenge;
-	} else {
-	    throw new WebApplicationException(Response.status(Status.BAD_REQUEST).build());
-	}
-    }
-
-    @Path("{email}/accepted")
-    @GET
-    @Produces(MediaType.APPLICATION_JSON)
-    public Challenge getAcceptedChallenge(@PathParam("email") String email) {
-	User user = em.find(User.class, email);
-	return user.getCurrentChallenge();
-    }
-
-    @Path("{email}/{challengeID}/accept")
-    @PUT
-    @Transactional
-    public Response acceptDailyChallenge(@PathParam("email") String email, @PathParam("challengeID") int id) {
-	User user = em.find(User.class, email);
-
-	//check if user has active challenge
-	if (user.getCurrentChallenge() != null && user.getCurrentChallenge().getId() == id) {
-	    return Response.status(Status.BAD_REQUEST).build();
-	}
-
-	if (user.getDailyChallenges().getFirst().getId() == id) {
-	    user.setCurrentChallenge(user.getDailyChallenges().getFirst());
-	    em.persist(user);
-	    return Response.ok().build();
-	}
-
-	if (user.getDailyChallenges().getSecond().getId() == id) {
-	    user.setCurrentChallenge(user.getDailyChallenges().getSecond());
-
-	    return Response.ok().build();
-	}
-
-	if (user.getDailyChallenges().getThird().getId() == id) {
-	    user.setCurrentChallenge(user.getDailyChallenges().getThird());
-	    em.persist(user);
-	    return Response.ok().build();
-	}
-	return Response.status(Status.BAD_REQUEST).build();
-    }
-
-    @Path("{email}/complete")
-    @PUT
-    @Transactional
-    public Response completeDailyChallenge(@PathParam("email") String email) {
-	//if the challenge is user.currentchallenge then remove it & add to completedchallenge 
-	User user = em.find(User.class, email);
-	
-	if(user.getCurrentChallenge() == null) {
-	    throw new WebApplicationException(Response.status(Status.BAD_REQUEST).build());
-	}
-	
-	user.getCompletedChallenges().add(user.getCurrentChallenge());
-	user.setCurrentChallenge(null);
-	em.persist(user);
-	return Response.ok().build();
-    }
 }
