2f830329495c6cbbd6e0ac8110ce71a2bf0ccde3
Mathias Bulté <bulte.mathias@gmail.com>
2015-11-03 15:38:29 +0100

    JWT Bugfix

--- Web/app/scripts/controllers/dashboard.js
+++ Web/app/scripts/controllers/dashboard.js
@@ -2,10 +2,11 @@
  * Created by wannes on 9/10/2015.
  */
 angular.module('eva').controller('DashboardCtrl',
-    ['$scope', '$location', 'auth', '$http',
-    function ($scope, $location, auth, $http) {
+    ['$scope', '$state', 'auth', '$http',
+    function ($scope, $state, auth, $http) {
     $scope.user = {'firstname': 'voornaam', 'lastname': 'achternaam', 'avatar': 'http://placehold.it/150x150'};
     if (auth.isLoggedIn()) {
     } else {
+        $state.go('login');
     }
 }]);
