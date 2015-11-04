3932951d962cdf19d3c1d7394fad75ac68011113
Mathias Bulté <bulte.mathias@gmail.com>
2015-11-04 19:15:18 +0100

    modal toegevoegd voor detail van challenges + details van challenge uit back-end halen

--- Web/app/scripts/controllers/dashboard.js
+++ Web/app/scripts/controllers/dashboard.js
@@ -5,35 +5,62 @@
 var app = angular.module('eva');
 
 app.controller('DashboardCtrl',
-    ['$scope', 'auth',
-        function ($scope, auth) {
+    ['$scope', 'auth', '$http',
+        function ($scope, auth, $http) {
             $scope.user = {
                 'firstname': 'voornaam',
                 'lastname': 'achternaam',
                 'avatar': 'http://placehold.it/150x150'
-            }
-
-            ;
+            };
+            $scope.challenges = {};
+            /*
+             Mock data
+             */
+            /*
             $scope.challenges = [
                 {
+                    id: '230',
                     title: 'test',
                     imageUrl: 'images/challenges/pompoensoep_kokoscreme_basilicum.jpg'
                 },
                 {
+                    id: '945',
                     title: 'test',
                     imageUrl: 'images/challenges/knolseldersoep.jpg'
                 },
                 {
+                    id: '32',
                     title: 'test',
                     imageUrl: 'images/challenges/knolseldersoep.jpg'
-                }];
+                }];*/
+            $scope.selectedIndex = 0;
+            $scope.selectedChallenge = function () {
+                return $scope.challenges[$scope.selectedIndex];
+            }
+            $scope.challengeClicked = function (arrayId) {
+                $scope.selectedIndex = arrayId;
+                $scope.loadChallengeData();
+            }
+            $scope.detailedChallenge = {};
 
+            $scope.loadChallengeData = function () {
+                $scope.detailedChallenge = {
+                    id: 2,
+                    title: 'Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus',
+                    description: "Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus Maak Winterse conchiglie met veggiegehakt, boerenkool en pompoensaus "
+                };
+
+                $http.get("http://bitcode.io:8080/backend/api/challenges/" + $scope.selectedChallenge().id).success(function (data, status, headers, config) {
+                    //$scope.challenges = data;
+                    console.log(data);
+                });
+            }
             if (auth.isLoggedIn()) {
-                /*
-                 $http.get("http://bitcode.io:8080/backend/api/challenges/daily").success(function (data, status, headers, config) {
+                ""
+                $http.get("http://bitcode.io:8080/backend/api/challenges/daily").success(function (data, status, headers, config) {
                  $scope.challenges = data;
                  console.log($scope.challenges[0]);
-                 });*/
+                 });
 
                 //challenges.getDailyChallenges();
                 //console.log(challenges.getDailyChallenges());
