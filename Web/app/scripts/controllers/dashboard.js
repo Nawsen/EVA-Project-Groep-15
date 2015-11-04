87fae8f570047c0ec8994f2336411ebf450bdab7
Mathias Bulté <bulte.mathias@gmail.com>
2015-11-04 16:20:45 +0100

    URLs terug veranderd naar bitcode.io, bugs gemaakt met ngDialog, è in crème veranderd naar creme voor image urls.

--- Web/app/scripts/controllers/dashboard.js
+++ Web/app/scripts/controllers/dashboard.js
@@ -1,25 +1,44 @@
 /**
  * Created by wannes on 9/10/2015.
  */
-angular.module('eva').controller('DashboardCtrl',
-    ['$scope', '$state', 'auth', '$http', 'challenges',
-        function ($scope, $state, auth, $http, challenges) {
+
+var app = angular.module('eva');
+
+app.controller('DashboardCtrl',
+    ['$scope', 'auth',
+        function ($scope, auth) {
             $scope.user = {
                 'firstname': 'voornaam',
                 'lastname': 'achternaam',
                 'avatar': 'http://placehold.it/150x150'
             }
+
             ;
-            $scope.challenges = {};
+            $scope.challenges = [
+                {
+                    title: 'test',
+                    imageUrl: 'images/challenges/pompoensoep_kokoscreme_basilicum.jpg'
+                },
+                {
+                    title: 'test',
+                    imageUrl: 'images/challenges/knolseldersoep.jpg'
+                },
+                {
+                    title: 'test',
+                    imageUrl: 'images/challenges/knolseldersoep.jpg'
+                }];
+
             if (auth.isLoggedIn()) {
-                $http.get("http://ts.wannesvandorpe.be:666/backend/api/challenges/daily").success(function (data, status, headers, config) {
-                    $scope.challenges = data;
-                    console.log($scope.challenges[0]);
-                });
+                /*
+                 $http.get("http://bitcode.io:8080/backend/api/challenges/daily").success(function (data, status, headers, config) {
+                 $scope.challenges = data;
+                 console.log($scope.challenges[0]);
+                 });*/
 
                 //challenges.getDailyChallenges();
-                console.log(challenges.getDailyChallenges());
+                //console.log(challenges.getDailyChallenges());
             } else {
                 $state.go('login');
             }
-        }]);
+        }]
+);
--- Web/app/scripts/factories/challenges.js
+++ Web/app/scripts/factories/challenges.js
@@ -11,7 +11,7 @@ app.factory('challenges', ['$http', 'auth', function ($http, auth) {
         }
         };
 
-        $http.get("http://ts.wannesvandorpe.be:666/backend/api/challenges/daily").success(function(data, status, headers, config) {
+        $http.get("http://bitcode.io:8080/backend/api/challenges/daily").success(function(data, status, headers, config) {
             challenges.challenges = data;
         });
         return challenges.challenges;
